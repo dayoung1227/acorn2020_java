4679ebeac77966cd3a39541c90f9f9227269b953
dayoung1227 <81479945+dayoung1227@users.noreply.github.com>
2021-05-03 16:10:12 +0900

    study added

--- Step02_DataType/src/test/main/MainClass02.java
+++ Step02_DataType/src/test/main/MainClass02.java
@@ -10,7 +10,7 @@ public class MainClass02 {
 		//국어 점수와 영어 점수의 평균을 구해서 변수에 담고 그 결과를 콘솔창에 출력해 보세요.
 		
 		// int type  과  double type 을 연산하면 결과는 double type 이다. 
-		double ave=(kor+eng)/2.0;
+		double ave=(kor+eng)/2.0; //2.0d
 		
 		System.out.println("평균:"+ave);
 	}
